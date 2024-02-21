eb538d45259e5a52b6972e698ab7bca4f90eada2
Ahmed khaled <ahmedkh8907@gmail.com>
2024-02-21 17:49:12 +0200

    expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,147 +12,39 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Responsive UI',
       theme: ThemeData.dark(),
-      home: const LayoutBuilderEx(),
+      home: const HomeView(),
     );
   }
 }
 
-class LayoutBuilderEx extends StatelessWidget {
-  const LayoutBuilderEx({super.key});
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          log((constraints.maxWidth.floor().toString()));
-          if (constraints.maxWidth <= 500) {
-            return const MobileLayout();
-          } else if (constraints.maxWidth <= 1020) {
-            return const TabletLayout();
-          } else {
-            return const WebLayout();
-          }
-        },
-      ),
-    );
-  }
-}
-
-class MobileLayout extends StatelessWidget {
-  const MobileLayout({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => DetailsPage(number: index + 1),
-              ),
-            );
-          },
-          child: Container(
-            color: Colors.redAccent,
-            margin: const EdgeInsets.only(bottom: 16),
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Center(
-              child: Text('${index + 1}'),
+      appBar: AppBar(automaticallyImplyLeading: true),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.redAccent,
             ),
           ),
-        );
-      },
-    );
-  }
-}
-
-class TabletLayout extends StatelessWidget {
-  const TabletLayout({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 7,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 5,
-      ),
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => DetailsPage(number: index + 1),
-              ),
-            );
-          },
-          child: Container(
-            color: Colors.redAccent,
-            margin: const EdgeInsets.only(bottom: 16),
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Center(
-              child: Text('${index + 1}'),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.greenAccent,
             ),
           ),
-        );
-      },
-    );
-  }
-}
-
-class WebLayout extends StatelessWidget {
-  const WebLayout({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        childAspectRatio: 7,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 5,
-      ),
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => DetailsPage(number: index + 1),
-              ),
-            );
-          },
-          child: Container(
-            color: Colors.redAccent,
-            margin: const EdgeInsets.only(bottom: 16),
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Center(
-              child: Text('${index + 1}'),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.blueAccent,
             ),
           ),
-        );
-      },
-    );
-  }
-}
-
-class DetailsPage extends StatelessWidget {
-  final int number;
-  const DetailsPage({super.key, required this.number});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text(
-          number.toString(),
-        ),
+        ],
       ),
     );
   }
