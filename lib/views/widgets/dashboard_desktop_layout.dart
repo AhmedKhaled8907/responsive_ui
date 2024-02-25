0ac978756baf5e07f204e108d87877b593bbdd0e
Ahmed khaled <ahmedkh8907@gmail.com>
2024-02-25 21:16:31 +0200

    user info list tile done

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_ui/views/widgets/custom_drawer.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Row(
+      children: [
+        Expanded(child: CustomDrawer()),
+        // Expanded(flex: 3, child: Container(color: Colors.redAccent)),
+        // Expanded(flex: 2, child: Container(color: Colors.greenAccent)),
+      ],
+    );
   }
 }
