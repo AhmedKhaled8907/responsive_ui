e1ae4614ac4d8210bb170c34cb796fb57df57154
Ahmed khaled <ahmedkh8907@gmail.com>
2024-02-25 21:48:56 +0200

    display drawer item

---
+++ lib/models/drawer_item_model.dart
@@ -0,0 +1,18 @@
+import 'package:responsive_ui/utils/app_images.dart';
+
+class DrawerItemModel {
+  final String image, title;
+
+  DrawerItemModel({
+    required this.image,
+    required this.title,
+  });
+}
+
+List<DrawerItemModel> draweritemsList = [
+  DrawerItemModel(image: Assets.imagesDashboard, title: 'Dashboard'),
+  DrawerItemModel(image: Assets.imagesMyTransctions, title: 'My Transaction'),
+  DrawerItemModel(image: Assets.imagesStatistics, title: 'Statistics'),
+  DrawerItemModel(image: Assets.imagesWalletAccount, title: 'Wallet Account'),
+  DrawerItemModel(image: Assets.imagesMyInvestments, title: 'My Investments'),
+];
--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_ui/models/drawer_item_model.dart';
 import 'package:responsive_ui/utils/app_images.dart';
+import 'package:responsive_ui/views/widgets/drawer_item.dart';
 import 'package:responsive_ui/views/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -9,12 +11,25 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Column(
+      child: Column(
         children: [
-          UserInfoListTile(
+          const UserInfoListTile(
             image: Assets.imagesAvatar3,
             title: 'Lekan Okeowo',
             subtitle: 'demo@gmail.com',
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: draweritemsList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return DrawerItem(
+                drawerItemModel: draweritemsList[index],
+              );
+            },
           ),
         ],
       ),
