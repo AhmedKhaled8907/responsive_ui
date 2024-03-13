0d169d4e8b58fe89b8466c518b67158f472ca178
Ahmed khaled <ahmedkh8907@gmail.com>
2024-03-13 03:15:41 +0200

    my card done

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:responsive_ui/utils/app_images.dart';
 import 'package:responsive_ui/views/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_ui/views/widgets/custom_drawer/custom_drawer.dart';
+
+import 'my_card/my_card.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -29,24 +29,3 @@ class DashboardDesktopLayout extends StatelessWidget {
   }
 }
 
-class MyCard extends StatelessWidget {
-  const MyCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 420 / 215,
-      child: Container(
-        decoration: ShapeDecoration(
-          image: const DecorationImage(
-            image: AssetImage(Assets.imagesCardBackground1),
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-          color: const Color(0xff4eb7f2),
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/views/widgets/my_card/my_card.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../utils/app_images.dart';
+import '../../../utils/app_styles.dart';
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 420 / 215,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        decoration: ShapeDecoration(
+          image: const DecorationImage(
+            image: AssetImage(Assets.imagesCardBackground1),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          color: const Color(0xff4eb7f2),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: const EdgeInsets.all(0),
+              title: Text(
+                'Card Name',
+                style: AppStyles.styleRegular12.copyWith(color: Colors.white),
+              ),
+              subtitle: Text(
+                'Syah Bandi',
+                style: AppStyles.styleMedium16.copyWith(color: Colors.white),
+              ),
+              trailing: SvgPicture.asset(
+                Assets.imagesGallery,
+                height: 20,
+                width: 20,
+              ),
+            ),
+            const Expanded(child: SizedBox()),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  '0918 8124 0042 8129',
+                  style: AppStyles.styleMedium16.copyWith(color: Colors.white),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  '12/20 - 124',
+                  style: AppStyles.styleRegular14.copyWith(color: Colors.white),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
