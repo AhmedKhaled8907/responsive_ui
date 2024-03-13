5142ce1276915587254c7662762885f7555fba71
Ahmed khaled <ahmedkh8907@gmail.com>
2024-03-13 20:15:15 +0200

    create dot indicator

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_ui/views/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_ui/views/widgets/custom_drawer/custom_drawer.dart';
+import 'package:responsive_ui/views/widgets/my_card/dots_indicator.dart';
 import 'package:responsive_ui/views/widgets/my_card/my_card_page_view.dart';
-
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -21,7 +21,13 @@ class DashboardDesktopLayout extends StatelessWidget {
         Expanded(
           child: Padding(
             padding: EdgeInsets.only(right: 32),
-            child: MyCardPageView(),
+            child: Column(
+              children: [
+                MyCardPageView(),
+                SizedBox(height: 16),
+                DotsIndicator(),
+              ],
+            ),
           ),
         ),
       ],
