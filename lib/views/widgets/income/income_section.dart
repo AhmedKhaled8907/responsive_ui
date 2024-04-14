4f73d11b8c8b61868895019413e3564749600dab
Ahmed khaled <ahmedkh8907@gmail.com>
2024-04-14 15:00:21 +0200

    income chart done

--- lib/views/widgets/income/income_section.dart
+++ lib/views/widgets/income/income_section.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:responsive_ui/views/widgets/custom_background_container.dart';
 import 'package:responsive_ui/views/widgets/income/income_section_header.dart';
+
+import 'income_chart.dart';
 
 class IncomeSection extends StatelessWidget {
   const IncomeSection({super.key});
@@ -8,10 +12,21 @@ class IncomeSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const CustomBackgroundContainer(
-        child: Column(
-      children: [
-        IncomeSectionHeader(),
-      ],
-    ));
+      child: Column(
+        children: [
+          IncomeSectionHeader(),
+          SizedBox(height: 16),
+          Row(
+            children: [
+              SizedBox(
+                height: 200,
+                width: 200,
+                child: IncomeChart(),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
