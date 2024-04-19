603ab8d85764aededeca65b6175dcda2573af185
Ahmed khaled <ahmedkh8907@gmail.com>
2024-04-19 15:29:58 +0200

    showing the custom drawer in the mobile layout

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_ui/utils/size_config.dart';
+import 'package:responsive_ui/views/widgets/custom_drawer/custom_drawer.dart';
 import 'package:responsive_ui/views/widgets/layouts/dashboard_mobile_layout.dart';
 import 'package:responsive_ui/views/widgets/layouts/dashboard_tablet_layout.dart';
 import 'package:responsive_ui/views/widgets/adaptive_layout.dart';
@@ -12,17 +13,17 @@ class DashboardView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xfff7f9fa),
+      drawer: MediaQuery.sizeOf(context).width < SizeConfig.tablet
+          ? const Drawer(
+              elevation: 0,
+              child: CustomDrawer(),
+            )
+          : null,
       appBar: MediaQuery.sizeOf(context).width < SizeConfig.tablet
           ? AppBar(
+              iconTheme: const IconThemeData(color: Colors.black),
               backgroundColor: const Color(0xfff7f9fa),
               elevation: 0,
-              leading: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.menu,
-                  color: Colors.black,
-                ),
-              ),
             )
           : null,
       body: AdaptiveLayout(
